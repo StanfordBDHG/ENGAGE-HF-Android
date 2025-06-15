7869920349399449210d8d6304b6361ce01946ea
eldcn <eldi.cano@hotmail.com>
2025-06-15 08:01:26 +0200

    Fix measurement date mapping for invalid unknown values (#9)
    # *Blood Pressure to observation mapping*
    
    ## :recycle: Current situation & Problem
    - Map measurement date to now Instant in case of invalid (non-set time
    yet) device time
    - Unit tests valid and invalid values
    
    
    ## :gear: Release Notes
    *Add a bullet point list summary of the feature and possible migration
    guides if this is a breaking change so this section can be added to the
    release notes.*
    *Include code snippets that provide examples of the feature implemented
    or links to the documentation if it appends or changes the public
    interface.*
    
    
    ## :books: Documentation
    *Please ensure that you properly document any additions in conformance
    to [Spezi Documentation
    Guide](https://github.com/StanfordSpezi/.github/blob/main/DOCUMENTATIONGUIDE.md).*
    *You can use this section to describe your solution, but we encourage
    contributors to document your reasoning and changes using in-line
    documentation.*
    
    
    ## :white_check_mark: Testing
    *Please ensure that the PR meets the testing requirements set by CodeCov
    and that new functionality is appropriately tested.*
    *This section describes important information about the tests and why
    some elements might not be testable.*
    
    
    ### Code of Conduct & Contributing Guidelines
    By creating and submitting this pull request, you agree to follow our
    [Code of
    Conduct](https://github.com/StanfordBDHG/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordBDHG/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordBDHG/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordBDHG/.github/blob/main/CONTRIBUTING.md).
    
    Co-authored-by: Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>

--- engagehf-modules/utils/src/main/kotlin/edu/stanford/bdh/engagehf/modules/utils/TimeProvider.kt
+++ engagehf-modules/utils/src/main/kotlin/edu/stanford/bdh/engagehf/modules/utils/TimeProvider.kt
@@ -2,10 +2,13 @@ package edu.stanford.bdh.engagehf.modules.utils
 
 import java.time.Instant
 import java.time.LocalTime
+import java.time.ZoneOffset
+import java.time.ZonedDateTime
 import javax.inject.Inject
 
 class TimeProvider @Inject constructor() {
     fun currentTimeMillis(): Long = nowInstant().toEpochMilli()
-    fun nowInstant() = Instant.now()
-    fun nowLocalTime() = LocalTime.now()
+    fun nowInstant(): Instant = Instant.now()
+    fun nowLocalTime(): LocalTime = LocalTime.now()
+    fun currentOffset(): ZoneOffset = ZonedDateTime.now().offset
 }
